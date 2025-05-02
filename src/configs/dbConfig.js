478768ae8cdbd613fbe79cdb59a79fe503325b99
ashuu7089 <ashupateriya2412@gmail.com>
2025-05-02 18:59:38 +0530

    add file in configs

--- src/configs/dbConfig.js
+++ src/configs/dbConfig.js
@@ -1,10 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } = process.env
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
+  port: DB_PORT,
   logging: false,
   pool: {
     max: 5,
